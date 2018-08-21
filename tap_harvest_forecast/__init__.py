893e21810eb1ba5cfb852aee1510054adddda99a
Robert Benjamin <rotberj@gmail.com>
2018-08-21 16:26:12 -0700

    Limit scope of Transformer class - Fix schema warning for people endpoint

--- tap_harvest_forecast/__init__.py
+++ tap_harvest_forecast/__init__.py
@@ -2,7 +2,6 @@
 
 import datetime
 import os
-
 import requests
 
 import singer
@@ -37,7 +36,6 @@ class Auth:
 def get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
 
-
 def load_schema(entity):
     return utils.load_json(get_abs_path("schemas/{}.json".format(entity)))
 
@@ -48,19 +46,18 @@ def get_start(key):
 
     return STATE[key]
 
-
 def get_url(endpoint):
     return BASE_URL + endpoint
 
 @backoff.on_exception(
     backoff.expo,
     (requests.exceptions.RequestException),
-    max_tries=5,
-    giveup=lambda e: e.response is not None and 400 <= e.response.status_code < 500,
-    factor=2)
+    max_tries = 5,
+    giveup = lambda e: e.response is not None and 400 <= e.response.status_code < 500,
+    factor = 2)
 @utils.ratelimit(100, 15)
 
-def request(url, params=None):
+def request(url, params = None):
     params = params or {}
     access_token = AUTH.get_access_token()
     account_id = AUTH.get_account_id()
@@ -79,33 +76,29 @@ def append_times_to_dates(item, date_fields):
             if item.get(date_field):
                 item[date_field] += "T00:00:00Z"
 
-def sync_endpoint(endpoint, date_fields=None):
+def sync_endpoint(endpoint, date_fields = None):
     schema = load_schema(endpoint)
     bookmark_property = 'updated_at'
 
     singer.write_schema(endpoint,
                         schema,
                         ["id"],
-                        bookmark_properties=[bookmark_property])
+                        bookmark_properties = [bookmark_property])
 
     start = get_start(endpoint)
-
     url = get_url(endpoint)
     data = request(url)[endpoint]
     time_extracted = utils.now()
 
-    with Transformer() as transformer:
-        for row in data:
-            item = row
-            item = transformer.transform(item, schema)
-
+    for row in data:
+        with Transformer() as transformer:
+            item = transformer.transform(row, schema)
             append_times_to_dates(item, date_fields)
 
             if item[bookmark_property] >= start:
                 singer.write_record(endpoint,
                                     item,
-                                    time_extracted=time_extracted)
-
+                                    time_extracted = time_extracted)
                 utils.update_state(STATE, endpoint, item[bookmark_property])
 
     singer.write_state(STATE)
@@ -136,7 +129,6 @@ def main():
     except Exception as exc:
         LOGGER.critical(exc)
         raise exc
-
 
 if __name__ == "__main__":
     main()
