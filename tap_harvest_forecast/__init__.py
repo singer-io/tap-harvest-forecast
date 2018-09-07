d930b0dff02b52b6b7bcb24aedfc692609a6cbc4
Dan Mosora <dan@stitchdata.com>
2018-09-07 17:32:54 +0000

    Adding refresh token logic to go by oauth flow

--- tap_harvest_forecast/__init__.py
+++ tap_harvest_forecast/__init__.py
@@ -13,20 +13,63 @@ SESSION = requests.Session()
 REQUIRED_CONFIG_KEYS = [
     "start_date",
     "account_id",
-    "access_token"
+    "client_id",
+    "client_secret",
+    "refresh_token"
 ]
 
 BASE_URL = "https://api.forecastapp.com/"
+BASE_ID_URL = "https://id.getharvest.com/api/v2/"
 CONFIG = {}
 STATE = {}
 AUTH = {}
 
 class Auth:
-    def __init__(self, account_id, access_token):
+    def __init__(self, account_id, client_id, client_secret, refresh_token):
         self._account_id = account_id
-        self._access_token = access_token
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._refresh_token = refresh_token
+        self._refresh_access_token()
+
+    @backoff.on_exception(
+        backoff.expo,
+        requests.exceptions.RequestException,
+        max_tries=5,
+        giveup=lambda e: e.response is not None and 400 <= e.response.status_code < 500,
+        factor=2)
+    def _make_refresh_token_request(self):
+        return requests.request('POST',
+                                url=BASE_ID_URL + 'oauth2/token',
+                                data={
+                                    'client_id': self._client_id,
+                                    'client_secret': self._client_secret,
+                                    'refresh_token': self._refresh_token,
+                                    'grant_type': 'refresh_token',
+                                },
+                                headers={"User-Agent": CONFIG.get("user_agent")})
+
+    def _refresh_access_token(self):
+        LOGGER.info("Refreshing access token")
+        resp_json = self._make_refresh_token_request().json()
+        expires_in_seconds = resp_json.get('expires_in', 17 * 60 * 60)
+        self._expires_at = utils.now() + datetime.timedelta(seconds=expires_in_seconds)
+
+        try:
+            self._access_token = resp_json['access_token']
+        except KeyError as key_err:
+            if resp_json.get('error'):
+                LOGGER.critical(resp_json.get('error'))
+            if resp_json.get('error_description'):
+                LOGGER.critical(resp_json.get('error_description'))
+            raise
+        LOGGER.info("Got refreshed access token")
 
     def get_access_token(self):
+        if self._access_token is not None and self._expires_at > utils.now():
+            return self._access_token
+
+        self._refresh_access_token()
         return self._access_token
 
     def get_account_id(self):
@@ -56,7 +99,6 @@ def get_url(endpoint):
     giveup = lambda e: e.response is not None and 400 <= e.response.status_code < 500,
     factor = 2)
 @utils.ratelimit(100, 15)
-
 def request(url, params = None):
     params = params or {}
     access_token = AUTH.get_access_token()
@@ -119,7 +161,7 @@ def main_impl():
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
     CONFIG.update(args.config)
     global AUTH # pylint: disable=global-statement
-    AUTH = Auth(CONFIG['account_id'], CONFIG['access_token'])
+    AUTH = Auth(CONFIG['account_id'], CONFIG["client_id"], CONFIG["client_secret"], CONFIG["refresh_token"])
     STATE.update(args.state)
     do_sync()
 
